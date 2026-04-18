dbaee9d55dd097889c9ac2d298cf1a4ed52e0abb
Thu Hang <vuthithuhang24cv@gmail.com>
2026-04-18 18:54:23 +0700

    Thêm Model SensorData, kết nối Firebase và tạo UI SensorCard

---
+++ android-app/app/src/main/java/com/htn/fishcare/dashboard/SensorData.kt
@@ -0,0 +1,8 @@
+package com.htn.fishcare.dashboard
+
+data class SensorData(
+    val temperature: Float = 0.0f,
+    val tds: Float = 0.0f,
+    val turbidity: Float = 0.0f,
+    val lastUpdated: Long = System.currentTimeMillis()
+)
